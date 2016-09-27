a0e004932531009ee9f0467cb3a613238ce73da4
mmacedoeu <bar4ka@gmail.com>
2016-09-26 21:23:14 -0300

    adhitzads adsptp

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","568354ddcf4d80454dba1b491e7c8154"],["/src/my-app.html","05fa3296244f9d24fd4d980d19ad21e4"],["/src/my-pending.html","58af1214181516e2d48d57d76d654ad7"],["/src/my-view1.html","d17a10de0281baf83ee8bd2072910cb7"],["/src/my-view2.html","11fa0b86d55a89686377d68413267ae9"],["/src/my-view3.html","8cbb642c709f68bc8b078f69c824e326"],["/src/my-view404.html","b9a28746f736435f74b42f65b65b4eac"]];
+var PrecacheConfig = [["/index.html","4541b8aa351ccae2ab44c4d1191f9a46"],["/src/my-app.html","05fa3296244f9d24fd4d980d19ad21e4"],["/src/my-pending.html","58af1214181516e2d48d57d76d654ad7"],["/src/my-view1.html","af862b3dbd1ad1e5a9938b5262455dc1"],["/src/my-view2.html","11fa0b86d55a89686377d68413267ae9"],["/src/my-view3.html","dc9f4289c9f9da73a6125eaab368536f"],["/src/my-view404.html","b9a28746f736435f74b42f65b65b4eac"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
